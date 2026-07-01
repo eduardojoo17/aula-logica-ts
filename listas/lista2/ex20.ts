2402778eb3b24d5682ad68c9e713aa2732881037
João Eduardo Paiva Da Costa <pet0160428@aluno.firjansenaisesi.com.br>
2026-07-01 18:17:51 -0300

    desvio

--- listas/lista2/ex20.ts
+++ listas/lista2/ex20.ts
@@ -1,6 +1,16 @@
 const pesoA: number = Number(prompt("Digite o seu peso em kg :"));
 const alturaA: number = Number(prompt("Digite a sua altura em metros:"));
 
-const imcA: number = peso / (altura * altura);
+const imcA: number = pesoA / (alturaA * alturaA);
 
-alert(`O seu IMC é: ${imc.toFixed(2)}`);
+alert(`O seu IMC é: ${imcA.toFixed(2)}`);
+
+if (imcA < 18.5) {
+  alert(`abaixo`);
+} else if (imcA < 25) {
+  alert(`normal`);
+} else if (imcA < 30) {
+  alert(`sobrepeso`);
+} else if (imcA >= 30) {
+  alert("obesidade");
+}
