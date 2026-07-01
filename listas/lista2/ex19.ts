bcb7f875e2c28caa955e18ae481e828f7cb5cb07
João Eduardo Paiva Da Costa <pet0160428@aluno.firjansenaisesi.com.br>
2026-06-30 21:31:06 -0300

    novo2

--- listas/lista2/ex19.ts
+++ listas/lista2/ex19.ts
@@ -1,3 +1,13 @@
 const numCal: number = Number(prompt("digite o numero:"));
 const numCal2: number = Number(prompt("digite outro numero:"));
 const numOp: string = prompt("Digite a operação") ?? "0";
+
+if (numOp === "-") {
+  alert(` ${numCal - numCal2} `);
+} else if (numOp === "+") {
+  alert(` ${numCal + numCal2}`);
+} else if (numOp === "*") {
+  alert(` ${numCal * numCal2} `);
+} else if (numOp === "/") {
+  alert(` ${numCal / numCal2} `);
+}
---
+++ listas/lista2/ex20.ts
@@ -0,0 +1,6 @@
+const pesoA: number = Number(prompt("Digite o seu peso em kg :"));
+const alturaA: number = Number(prompt("Digite a sua altura em metros:"));
+
+const imcA: number = peso / (altura * altura);
+
+alert(`O seu IMC é: ${imc.toFixed(2)}`);
